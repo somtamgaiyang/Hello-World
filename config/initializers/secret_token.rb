7fc97c0e4f740bb6357e06ac3574b668fccec815
Scott Gelb <scotthg@hotmail.com>
2011-05-29 12:52:09 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScottgelbWww::Application.config.secret_token = '6da75242084f1f9fe6aa64d4eb70a7fbb48548028ee5e3b65ca374169ef15f36f95d24fbcaba0bf5528830c484aae9538d370d7839ac8744c9b37313d2385ab3'
